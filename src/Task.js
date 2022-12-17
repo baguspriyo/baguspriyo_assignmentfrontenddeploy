8734c48abbe2336b27271869274af4892f04d700
baguspriyo <baguspriyo727@gmail.com>
2022-12-17 14:05:46 +0700

    Upload Project ke Git

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "YOURDEPLOYURL" // TODO: replace this
+const studentName = "Bagus Priyo Utomo" // TODO: replace this
+const studentId = "FE08" // TODO: replace this
+
+export { url, studentName, studentId }
